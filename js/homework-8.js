0ef78bb6b41a703cd373170a2bb9936e0a451fd7
denicccccc <goncharov.den0410@gmail.com>
2025-04-11 21:05:31 +0200

    Update homework-8.js

---
+++ js/homework-8.js
@@ -0,0 +1,46 @@
+// const result = 5+5+'5';
+// console.log(result)
+// console.log(typeof result)
+
+// const email = 'ahmed.alie0643@gmail.com';
+// console.log(email.includes('@'));
+// const count = email.length;
+// console.log(count)
+
+// const text1 = 'My';
+// const text2 = 'name';
+// const text3 = 'is';
+// const instruction = 'Ведіть своє ім*я';
+// const userName = prompt(instruction)
+// const fullName = `${text1} ${text2} ${text3} ${userName}`;
+// console.log(fullName)
+
+// const workerName = prompt(instruction);
+// const payment = 300;
+// alert(`Дякуємо, ${workerName}! До сплати ${payment} гривень`);
+
+// const MyFullName = 'Coco Jambo';
+// console.log(MyFullName.indexOf(' '))
+
+// const blacklistedWord1 = 'спам';
+// const blacklistedWord2 = 'розпродаж';
+// const string1 = 'Привіт, я Ілон Маск, це не спам, пропоную тобі мільйон!';
+// const lowercaseSumbol1 = string1.toLowerCase();
+// console.log(lowercaseSumbol1)
+// const scamOrNot = lowercaseSumbol1.includes('розпродаж') || lowercaseSumbol1.includes('спам');
+// console.log(scamOrNot)
+// alert(`string1 має black listed word = ${scamOrNot}`)
+
+// const string2 = 'Найбільший РОЗПРОДАЖ цього тижня, не пропустіть!';
+// const lowercaseSumbol2 = string2.toLowerCase();
+// console.log(lowercaseSumbol2)
+// const scamOrNot2 = lowercaseSumbol2.includes('розпродаж') || lowercaseSumbol2.includes('спам');
+// console.log(scamOrNot2)
+// alert(`string2 має black listed word = ${scamOrNot2}`)
+
+// const string3 = 'Рекламна кампанія #jsIsAwesome';
+// const lowercaseSumbol3 = string3.toLowerCase();
+// console.log(lowercaseSumbol3)
+// const scamOrNot3 = lowercaseSumbol3.includes('розпродаж') || lowercaseSumbol3.includes('спам');
+// console.log(scamOrNot3)
+// alert(`string3 має black listed word = ${scamOrNot3}`)
